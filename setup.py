d2c74fadabc4d82f7e429d5a17d36effdb0ead0d
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-06-29 15:15:57 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -11,23 +11,23 @@ directory = os.path.split(__file__)[0]
 print(directory)
 
 
-class InstallLocalPackage(install):
-    def run(self):
-        print('PREEE COMPILED ROBOTS')
-        print('PREEE COMPILED ROBOTS')
-        print('PREEE COMPILED ROBOTS')
-        print('PREEE COMPILED ROBOTS')
-        install.run(self)
-        print(directory)
-        # subprocess.call(f"cd {directory}/rokin/Robots/Justin19/cpp; python setup.py develop", shell=True)
-        # subprocess.call(f"cd {directory}/rokin/Robots/JustinArm07/cpp; python setup.py develop", shell=True)
-        # subprocess.call(f"cd {directory}/rokin/Robots/JustinFinger03/cpp; python setup.py develop", shell=True)
-        # subprocess.call(f"cd {directory}/rokin/Robots/JustinHand12/cpp; python setup.py develop", shell=True)
-        # subprocess.call(f"cd {directory}/rokin/Robots/JustinHand12Cal/cpp; python setup.py develop", shell=True)
-        print('COMPILED ROBOTS')
-        print('COMPILED ROBOTS')
-        print('COMPILED ROBOTS')
-        print('COMPILED ROBOTS')
+# class InstallLocalPackage(install):
+#     def run(self):
+#         print('PREEE COMPILED ROBOTS')
+#         print('PREEE COMPILED ROBOTS')
+#         print('PREEE COMPILED ROBOTS')
+#         print('PREEE COMPILED ROBOTS')
+#         install.run(self)
+#         print(directory)
+#         # subprocess.call(f"cd {directory}/rokin/Robots/Justin19/cpp; python setup.py develop", shell=True)
+#         # subprocess.call(f"cd {directory}/rokin/Robots/JustinArm07/cpp; python setup.py develop", shell=True)
+#         # subprocess.call(f"cd {directory}/rokin/Robots/JustinFinger03/cpp; python setup.py develop", shell=True)
+#         # subprocess.call(f"cd {directory}/rokin/Robots/JustinHand12/cpp; python setup.py develop", shell=True)
+#         # subprocess.call(f"cd {directory}/rokin/Robots/JustinHand12Cal/cpp; python setup.py develop", shell=True)
+#         print('COMPILED ROBOTS')
+#         print('COMPILED ROBOTS')
+#         print('COMPILED ROBOTS')
+#         print('COMPILED ROBOTS')
 
 
 setup(
@@ -46,6 +46,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    cmdclass={'install': InstallLocalPackage,
-              'develop': InstallLocalPackage}
+    # cmdclass={'install': InstallLocalPackage,
+    #           'develop': InstallLocalPackage}
 )
